0d446040a643ff28fb7d1fc909118ab2dc5f070b
Kota1021 <ko_matsumoto@yumemi.co.jp>
2024-06-11 17:33:58 +0900

    use Assets

--- lib/weather_kind.dart
+++ lib/weather_kind.dart
@@ -1,11 +1,12 @@
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:flutter_training/gen/assets.gen.dart';
 
 enum WeatherKind { sunny, cloudy, rainy }
 
 extension WeatherKindIcon on WeatherKind {
   SvgPicture get svgImage => switch (this) {
-        WeatherKind.sunny => SvgPicture.asset('images/Sunny.svg'),
-        WeatherKind.cloudy => SvgPicture.asset('images/Cloudy.svg'),
-        WeatherKind.rainy => SvgPicture.asset('images/Rainy.svg'),
+        WeatherKind.sunny => Assets.images.sunny.svg(),
+        WeatherKind.cloudy => Assets.images.cloudy.svg(),
+        WeatherKind.rainy => Assets.images.rainy.svg(),
       };
 }
